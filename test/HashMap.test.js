4ae6555c9c43bfc17a780537b2d30e1376efcbc0
krig6 <ej.ldsm@gmail.com>
2026-02-09 14:39:02 +0800

    test(HashMap): verify default load factor for new HashMap
    - Verifies that a new `HashMap` instance has the default load factor of `0.75`

---
+++ test/HashMap.test.js
@@ -0,0 +1,15 @@
+import HashMap from "../src/classes/HashMap.js"
+
+describe('HashMap class', () => {
+  let hashmap
+
+  beforeEach(() => {
+    hashmap = new HashMap()
+  })
+
+  describe('hashmap load factor', () => {
+    test('should have a default load factor 0.75', () => {
+      expect(hashmap.loadFactor).toBe(0.75)
+    })
+  })
+})
